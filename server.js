6744014eb1ac3c26bb41a827e8a0bec82b755ab2
Lily Clark <lilyclarkcodes@gmail.com>
2022-11-18 23:07:55 -0800

    get quote from url

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ console.log("May Node be with you");
 
 const express = require('express');
 const app = express();
+const PORT = 8000
 
-app.listen(3000,function() {
-    console.log('listening on 3000')
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Listening on ${PORT}`)
 })
 
 // We normally abbreviate `request` to `req` and `response` to `res`.
 app.get('/', (req, res) => {
     // do something here, eg. simply res.send('Hello World')
     //below you send the html file found in the directory listed
-    res.sendFile('/Users/lilyclark/desktop/portfolio/CRUD-Express-MongoDB-practice-app' + "/index.html")
+    res.sendFile(__dirname + "/index.html")
   })
+
+//use the url to obtain the quote. Will change to input. Will need to consider spaces
+app.get('/api/:quote', (req, res)=> {
+    const quote = req.params.quote
+    console.log(quote)
+})
